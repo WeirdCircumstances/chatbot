e8e7c5be3f75e46283757c8671c7f924bb0e38ce
Benjamin Beuster <ben@Rechenknecht.fritz.box>
2019-12-05 12:31:45 +0100

    Chatbot for CODE{affair}

--- chatbot.py
+++ chatbot.py
@@ -1,6 +1,6 @@
 '''
 CODY - TextBot
-A simple NLTK Text Chatbot for the "Tag der Talente" workshop 2019 
+A simple NLTK Text Chatbot for the CODE{affair} workshop 2019 
 based on an example script from Parul Pandey.
 '''
 import io
